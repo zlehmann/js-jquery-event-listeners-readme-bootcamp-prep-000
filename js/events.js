9d834ec4146505041b4edb3a5acbbe5c1ddb1b3b
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-29 18:54:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,9 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('')
+  $('form').on('submit',function() {
+    alert('Your form is going to be submitted now.')
+  })
 }
 
 $(document).ready(function(){
